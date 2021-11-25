7ee347903b13202202a0489468a108cbdd6c4363
mannidev <kjoedev@gmail.com>
2021-11-25 11:11:39 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './style.css';
 
-import React, { useState } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import AppContainer from './components/app-container/AppContainer';
@@ -8,20 +8,7 @@ import Dashboard from './pages/dashboard/Dashboard';
 import Expense from './pages/expense/Expense';
 import Login from './pages/login/Login';
 
-
-import { ExpensesModel } from './model/ExpensesModel';
-import * as Labels from './labels';
-
-const expenses = ExpensesModel.getInstance();
-
 export default function App() {
-  const [expenseItems, setExpenseItems] = useState(expenses.getItems());
-
-  const onSaveExpenseItemHandler = (data) => {
-    expenses.addExpenseItem(data);
-    setExpenseItems([...expenses.getItems()]);
-  };
-
   return (
     <AppContainer>
       <Routes>
